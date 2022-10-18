4b2fe61f221db85ca060e43320a0300274419fc7
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-10-18 11:27:52 +0200

    import Handler2 old code

---
+++ app/Rules/MatchAccessKey.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\AccessKey;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+
+class MatchAccessKey implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        $match = false;
+        foreach (AccessKey::all() as $accessKey){
+            if(Hash::check($value,$accessKey->key)) {
+                $match = true;
+            }
+        }
+        return $match;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'The key is invalid';
+    }
+}
